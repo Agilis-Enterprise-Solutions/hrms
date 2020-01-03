4d8dda7778eaf5ef5184684db71804837a3437d8
jardosa <jardosa@live.com>
2020-01-03 13:36:44 +0800

    added candidate assessment py and xml

---
+++ hrms_v3_recruitment/models/candidate_assessment.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+from datetime import date, datetime, timedelta
+import logging
+import itertools
+import calendar
+from odoo.exceptions import ValidationError
+from num2words import num2words
+
+
+class Assessment(models.Model):
+    _name = "hr.assessment"
+
+    name = fields.Char(string="Test Name",
+                       required=True
+                       )
+    applicant_name = fields.Char(string="Applicant")
+    job_id = fields.Many2one('hr.job', string="Job Position")
+    test_type = fields.Selection(
+        string='Test Type',
+        selection=[
+            ('cognitive', 'Cognitive'),
+            ('aptitude', 'Aptitude'),
+            ('personality', 'Personality'),
+            ('skill', 'Skill'),
+            ('physical', 'Physical'),
+        ]
+    )
+    website = fields.Char(string="Source Website")
+    assessment_date = fields.Date(string="Date of Assessment")
+
+    number_of_items = fields.Integer(
+        string='Number of Items',
+    )
+    correct_items = fields.Integer(string="Correct Items")
+
+    result = fields.Selection(
+        string='Result',
+        selection=[
+            ('passed', 'Passed'),
+            ('failed', 'Failed')]
+    )
+    checked_by_id = fields.Many2one('hr.employee')
+    
