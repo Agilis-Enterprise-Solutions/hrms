8c201768e95a4fc70033244785581b29ec4e3966
jardosa <jardosa@live.com>
2020-02-13 18:11:16 +0800

    removed commented file

--- hrv3_compliance/models/infraction.py
+++ hrv3_compliance/models/infraction.py
@@ -504,65 +504,6 @@ class ActionHistory(models.Model):
         return True
 
 
-# class Suspension(models.Model):
-#     _name = 'create.suspension'
-#     _description = 'Create Suspension'
-
-#     status = [
-#         ('on_going', 'On Going'),
-#         ('completed', 'Completed')
-#     ]
-
-#     state = fields.Selection(
-#         string='Status',
-#         selection=status
-#     )
-#     emp_id = fields.Many2one('hr.employee', string="Offending Employee")
-#     infraction_id = fields.Many2one(
-#         'hr.infraction', string="Infraction Record")
-#     suspension_days = fields.Integer(string="Suspension Days")
-#     remaining_days = fields.Integer(
-#         string="Remaining Days", compute='compute_remaining_days')
-#     use_suspension_days = fields.Integer(
-#         string="Use Suspension", compute='compute_use_suspension_days')
-#     start_date = fields.Date(string="Start Date")
-#     end_date = fields.Date(string="End Date")
-#     history_ids = fields.Many2many('suspension.history', string="History",
-#     # domain="[('emp_id','=',emp_id)]"
-#     )
-
-#     @api.depends('use_suspension_days')
-#     def compute_remaining_days(self):
-#         for rec in self:
-#             suspension_days = rec.suspension_days
-#         self.remaining_days = suspension_days - rec.use_suspension_days
-#         return True
-
-#     @api.depends('start_date', 'end_date')
-#     def compute_use_suspension_days(self):
-#         if self.start_date and self.end_date:
-#             self.use_suspension_days = abs(
-#                 (self.start_date - self.end_date)).days
-#         return True
-
-#     @api.multi
-#     def create_suspension(self):
-#         self.state = "on_going"
-#         if self.remaining_days < 0:
-#             raise UserError(
-#                 _('Suspension days to be used must not be more than remaining suspension days.'))
-#         else:
-#             vals = {
-#                 'emp_id': self.emp_id.id,
-#                 'infraction_id': self.infraction_id.id,
-#                 'used_days': self.use_suspension_days,
-#                 'date_from': self.start_date,
-#                 'date_to': self.end_date,
-#                 'state': self.state,
-#             }
-#         self.env['suspension.history'].create(vals)
-
-
 """========================SUSPENSION HISTORY=======================
         ALL INSTANCES OF EMPLOYEE SUSPENSION IS RECORDED HERE
         THIS MAY BE USED FOR PAYROLL AND TIMEKEEPING PURPOSES
