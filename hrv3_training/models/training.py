e0f084ca5d49def550b3cfc69835f369e0478452
Raymund Martinez <zhaqenl@gmail.com>
2020-02-13 13:08:31 +0800

    Add fields to hrv3_training

--- hrv3_training/models/training.py
+++ hrv3_training/models/training.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from odoo import models, fields, api
 from logging import getLogger
+from datetime import date
 
 
 def log(**to_output):
@@ -14,27 +15,56 @@ class Training(models.Model):
 
     employee_id = fields.Many2one('hr.employee')
     employee_ids = fields.Many2many('hr.employee',
-                                    string='Employees Attending')
+                                    string='Attendees')
 
     training_name = fields.Char(required=True)
-    subject = fields.Char()
+    subject = fields.Char('Training Subject')
     training_type = fields.Selection([
         ('Online', 'Online'),
         ('Classroom', 'Classroom'),
         ('Self-paced', 'Self-paced')
-    ], string="Type")
+    ])
     date_completed = fields.Date()
     date = fields.Date()
     venue = fields.Char()
-    trainor = fields.Char()
-    hour = fields.Float()
+    trainor = fields.Char('Trainer')
     cost = fields.Float()
     sponsor = fields.Char()
+    organizer = fields.Char()
+    duration = fields.Integer('Duration (hr/s)')
+    start_date = fields.Date(required=True)
     acquire_certificate = fields.Boolean()
+    description = fields.Text()
+    venue = fields.Text()
+    on_premises = fields.Boolean()
     status = fields.Selection([
-        ('Ongoing', 'Ongoing'),
-        ('Completed', 'Completed')
-    ])
+        ('Proposed', 'Proposed'),
+        ('Scheduled', 'Scheduled'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled')
+    ], default='Proposed', compute="_set_status", store=True)
+
+    @api.multi
+    def done(self):
+        self.write({
+            'status': 'Completed'
+        })
+
+        return True
+
+    @api.multi
+    def cancel(self):
+        self.write({
+            'status': 'Cancelled'
+        })
+
+        return True
+
+    @api.depends('start_date')
+    def _set_status(self):
+        for rec in self:
+            if rec.start_date and (rec.start_date > date.today()):
+                rec.status = 'Scheduled'
 
 
 class Employee(models.Model):
