40760bf5d60912d333c1b109de7249fb065f4677
ralf-agilis <ralf@biz1global.com>
2020-03-02 14:37:47 +0800

    Add Work history module

---
+++ hrms_work_history/__manifest__.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+{
+    'name': "HRMS Work History",
+
+    'summary': """
+        HRMS Work History""",
+
+    'description': """
+        Work module module is part of employee 201
+    """,
+
+    'author': "John Ardosa, Raymund Martinez, and Ralf Cabarogias - Agilis Enterprise Solutions",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'HR',
+    'version': '0.1',
+
+    'depends': [
+        'base',
+        'hr',
+        'hrms_employee_201'
+    ],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/work_history.xml',
+
+    ],
+}
