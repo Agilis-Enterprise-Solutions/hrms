64db53c7b48221c8caeee49b3c19636ab11fe8c7
ralf-agilis <ralf@biz1global.com>
2020-03-13 15:32:44 +0800

    Add separation file field, add string name in every field

--- hrms_employee_exit/models/employee_exit.py
+++ hrms_employee_exit/models/employee_exit.py
@@ -6,9 +6,9 @@ from datetime import date
 class InheritEmployeeAddExit(models.Model):
     _inherit = 'hr.employee'
 
-    on_hold = fields.Boolean()
-    date_started = fields.Date()
-    date_exited = fields.Date()
+    on_hold = fields.Boolean('On Hold')
+    date_started = fields.Date('Date Started')
+    date_exited = fields.Date('Exit Date')
     years_of_service = fields.Char('Year(s) of Service',
                                    compute="_get_years_of_service")
     status = fields.Selection([
@@ -16,8 +16,9 @@ class InheritEmployeeAddExit(models.Model):
         ('Resigned', 'Resigned'),
         ('Retired', 'Retired'),
         ('Terminated', 'Terminated')
-    ])
-    exit_reason = fields.Text()
+    ], default='Active', string="Status")
+    exit_reason = fields.Text('Exit Reason')
+    separation_id = fields.Many2one('hr.separation','Separation File')
 
     @api.depends('date_started','date_exited')
     def _get_years_of_service(self):
