5c397aaf18d70b25a5e1e7318473c025ed417971
Raymund Martinez <zhaqenl@gmail.com>
2020-03-26 12:13:15 +0800

    Lint

--- hrms_separation_management/models/exit_clearance.py
+++ hrms_separation_management/models/exit_clearance.py
@@ -1,9 +1,10 @@
-from odoo import models, fields, api, _
+from odoo import models, fields, api
 from datetime import date
 from odoo.exceptions import UserError
 import logging
 
 _logger = logging.getLogger(__name__)
+
 
 class HRMSExitClearance(models.Model):
     _name = "hr.exit.clearance"
@@ -21,7 +22,7 @@ class HRMSExitClearance(models.Model):
                                   store=True)
     name = fields.Many2one('hr.employee', "Employee Name", required=True)
     job_id = fields.Many2one('hr.job', "Position",
-                                    related="name.job_id")
+                             related="name.job_id")
     separation_type = fields.Selection([
         ('resignation', 'Resignation'),
         ('terminated_company', 'Termination(Company Initiated)'),
@@ -30,11 +31,11 @@ class HRMSExitClearance(models.Model):
     ], string="Separation Type", compute="_get_separation_request",
                                         store=True)
     joined = fields.Date("Joined Date", compute="_get_separation_request",
-                                        store=True)
+                         store=True)
     relieved = fields.Date("Relieved Date", compute="_get_separation_request",
-                                        store=True)
+                           store=True)
     date_raised = fields.Date("Date Raised", compute="_get_separation_request",
-                                        store=True)
+                              store=True)
     clearance_details = fields.One2many('hr.exit.clearance.lines',
                                         'clearance_id',
                                         string="Clearance Deatils")
@@ -46,7 +47,7 @@ class HRMSExitClearance(models.Model):
 
     separation_parent_id = fields.Many2one('hr.separation',
                                            'Separation Parent ID',
-                                            ondelete="cascade")
+                                           ondelete="cascade")
 
     date_submitted = fields.Date("Date Submitted")
     submitted_by = fields.Many2one('res.users', 'Submitted By')
@@ -71,28 +72,32 @@ class HRMSExitClearance(models.Model):
             user_id = self.env['res.users'].browse(self._context.get('uid'))
             rec.submitted_by = user_id.id
 
-            duplicate = self.search([('name', '=', rec.name.id),
-                                     ('separation_parent_id', '=', rec.separation_parent_id.id),
-                                     ('state', '=', 'pending')])
+            duplicate = self.search([
+                ('name', '=', rec.name.id),
+                ('separation_parent_id', '=', rec.separation_parent_id.id),
+                ('state', '=', 'pending')
+            ])
             if duplicate:
                 raise UserError('''Duplicate Exit Clearance.
                                 Please Check the other records for Reference''')
 
-
             approver = self.env['hr.exit.clearance.approver'].search([
-                ('job_id','=',rec.job_id.id)])
+                ('job_id', '=', rec.job_id.id)
+            ])
 
             if approver:
                 for i in approver.approve_ids:
-                    val= {
+                    val = {
                         'name' : i.name.id,
                         'job_id' : i.job_id.id,
                         'department_id' : i.department_id.id,
-                        }
+                    }
                     data.append([0, 0, val])
 
-        return self.write({'clearance_details': data,
-                           'state': 'pending'})
+        return self.write({
+            'clearance_details': data,
+            'state': 'pending'
+        })
 
     @api.multi
     def clear(self):
@@ -102,19 +107,22 @@ class HRMSExitClearance(models.Model):
                 if rec.status == 'pending':
                     pending = True
 
-            if pending == False:
+            if not pending:
                 i.date_approved = date.today()
                 user_id = self.env['res.users'].browse(self._context.get('uid'))
                 i.approved_by = user_id.id
                 separation_req = self.env['hr.separation'].search([
-                    ('name','=',self.name.id),
-                    ('state','=','approve'),
-                    ('clearance','=',False)])
+                    ('name', '=', self.name.id),
+                    ('state', '=', 'approve'),
+                    ('clearance', '=', False)
+                ])
 
                 if separation_req:
                     separation_req.clearance = True
 
-                return self.write({'state': 'cleared'})
+                return self.write({
+                    'state': 'cleared'
+                })
             else:
                 raise UserError('Employee need to be approved in every Approver below')
 
@@ -123,9 +131,10 @@ class HRMSExitClearance(models.Model):
         for rec in self:
             if rec.name:
                 separation_req = rec.env['hr.separation'].search([
-                    ('name','=',rec.name.id),
-                    ('state','=','approve'),
-                    ('clearance','=',False)])
+                    ('name', '=',rec.name.id),
+                    ('state', '=', 'approve'),
+                    ('clearance', '=', False)
+                ])
                 if separation_req:
                     rec.decision = separation_req.state
                     rec.note = separation_req.note
@@ -135,6 +144,7 @@ class HRMSExitClearance(models.Model):
                     rec.relieved = separation_req.relieved
                     rec.date_raised = separation_req.date_raised
                     rec.separation_parent_id = separation_req.id
+
 
 class HRMSExitClearanceLines(models.Model):
     _name = "hr.exit.clearance.lines"
@@ -154,7 +164,6 @@ class HRMSExitClearanceApprover(models.Model):
     _name = "hr.exit.clearance.approver"
     _rec_name = "job_id"
 
-
     job_id = fields.Many2one('hr.job', "Job Position", required=True)
     approve_ids = fields.One2many('hr.exit.clearance.approver.lines',
                                   'approver_id',
@@ -164,8 +173,10 @@ class HRMSExitClearanceApprover(models.Model):
     def check_duplicate_true(self):
         for i in self:
             if i.job_id:
-                duplicate = self.search([('job_id', '=', i.job_id.id),
-                                         ('id', '!=', i.id)])
+                duplicate = self.search([
+                    ('job_id', '=', i.job_id.id),
+                    ('id', '!=', i.id)
+                ])
                 if duplicate:
                     raise UserError('''One Department is allowed.
                                     Cannot Create with same Department Approver''')
@@ -174,7 +185,8 @@ class HRMSExitClearanceApprover(models.Model):
 class HRMSExitClearanceApproverLines(models.Model):
     _name = "hr.exit.clearance.approver.lines"
 
-    approver_id = fields.Many2one('hr.exit.clearance.approver', ondelete="cascade")
+    approver_id = fields.Many2one('hr.exit.clearance.approver',
+                                  ondelete="cascade")
     name = fields.Many2one('hr.employee', "Name")
     job_id = fields.Many2one('hr.job', "Job Position", related="name.job_id")
     department_id = fields.Many2one('hr.department', "Department",
